86d4fb8c368387a4f56200e78802131a193c3f71
Timo Strunk <Timo.Strunk@nanomatch.com>
2020-02-06 11:17:38 +0100

    HTTP Server upgrades

--- SimStackServer/HTTPServer/HTTPServer.py
+++ SimStackServer/HTTPServer/HTTPServer.py
@@ -1,9 +1,13 @@
+import datetime
 import http.server
 import cgi
 import base64
 import json
 import logging
 import os
+import html
+import sys
+import io
 import time
 import urllib
 from functools import partial
@@ -12,6 +16,9 @@ from urllib.parse import urlparse, parse_qs
 import string
 import random
 from threading import Thread
+import SimStackServer.Data as DataDir
+
+
 
 
 def random_string(stringLength=10):
@@ -21,11 +28,12 @@ def random_string(stringLength=10):
 
 
 class CustomServerHandler(http.server.SimpleHTTPRequestHandler):
+    static_files = [
+        "/dirlist.css"
+    ]
     def __init__(self,*args,**kwargs):
         self._logger = logging.getLogger("HTTPServerHandler")
-        self._favicon = None
-        self._read_favicon_to_memory()
-
+        self._favicon = self._read_static_binary_file_to_memory("favicon.ico")
         super().__init__(*args,**kwargs)
 
         super().extensions_map.update({
@@ -40,13 +48,15 @@ class CustomServerHandler(http.server.SimpleHTTPRequestHandler):
             '.slr': 'text/plain'
         })
 
+    def _get_static_http_path(self):
+        data_dir = os.path.join(os.path.dirname(os.path.realpath(DataDir.__file__)), "static_http")
+        return data_dir
 
-    def _read_favicon_to_memory(self):
-        import SimStackServer.Data as Data
-        data_dir = os.path.dirname(os.path.realpath(Data.__file__))
-        favpath = os.path.join(data_dir,"favicon.ico")
+    def _read_static_binary_file_to_memory(self, filename):
+        data_dir = self._get_static_http_path()
+        favpath = os.path.join(data_dir, filename)
         with open(favpath,'rb') as infile:
-            self._favicon = infile.read()
+            return infile.read()
 
     def do_HEAD(self):
         self.send_response(200)
@@ -105,6 +115,17 @@ class CustomServerHandler(http.server.SimpleHTTPRequestHandler):
                 self.send_header("Content-Length", len(self._favicon)-1)
                 self.end_headers()
                 self.wfile.write(self._favicon)
+                return
+
+            if self.path in self.static_files:
+                ctype = self.guess_type(self.path)
+                mypath = self.path[1:]
+                data = self._read_static_binary_file_to_memory(mypath)
+                self.send_response(HTTPStatus.OK)
+                self.send_header("Content-type", ctype)
+                self.send_header("Content-Length", len(data) - 1)
+                self.end_headers()
+                self.wfile.write(data)
                 return
 
             f = self.send_head()
@@ -197,6 +218,94 @@ class CustomServerHandler(http.server.SimpleHTTPRequestHandler):
         getvars = parse_qs(urlparse(self.path).query)
 
         return getvars
+
+    @staticmethod
+    def human_readable_size(size, decimal_places=3):
+        for unit in ['B','KiB','MiB','GiB','TiB']:
+            if size < 1024.0:
+                break
+            size /= 1024.0
+        return f"{size:.{decimal_places}f}{unit}"
+
+    def list_directory(self, path):
+        """Helper to produce a directory listing (absent index.html).
+        Override of same file in server.py
+
+        Return value is either a file object, or None (indicating an
+        error).  In either case, the headers are sent, making the
+        interface the same as for send_head().
+
+        """
+        try:
+            list = os.listdir(path)
+        except OSError:
+            self.send_error(
+                HTTPStatus.NOT_FOUND,
+                "No permission to list directory")
+            return None
+        list.sort(key=lambda a: a.lower())
+        r = []
+        try:
+            displaypath = urllib.parse.unquote(self.path,
+                                               errors='surrogatepass')
+        except UnicodeDecodeError:
+            displaypath = urllib.parse.unquote(path)
+        displaypath = html.escape(displaypath, quote=False)
+        enc = sys.getfilesystemencoding()
+        title = 'Directory listing for %s' % displaypath
+        r.append('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" '
+                 '"http://www.w3.org/TR/html4/strict.dtd">')
+        r.append('<html>\n<head>')
+        r.append('<link rel="stylesheet" href="/dirlist.css" />')
+        r.append('<meta http-equiv="Content-Type" '
+                 'content="text/html; charset=%s">' % enc)
+        r.append('<title>%s</title>\n</head>' % title)
+        r.append('<body>\n<br/><center><b>Index of %s</b></center><br/>' % title)
+        r.append('<div class="list">')
+        r.append('<table summary="Directory Listing" cellpadding="0" cellspacing="0">')
+        r.append('<thead><tr><th class="n">Name</th><th class="m">Last Modified</th><th class="s">Size</th><th class="t">Type</th></tr></thead>')
+        #r.append('<thead><tr><th class="n">Name</th><th class="m">Last Modified</th><th class="t">Type</th></thead>')
+        r.append('<tbody>')
+        for name in list:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            filetype = "File"
+            lastmodified = "-"
+            filesize = ""
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+                filetype = "Link"
+            elif os.path.islink(fullname):
+                displayname = name + "@"
+                filetype = "Directory"
+                # Note: a link to a directory displays with @ and links with /
+            elif os.path.isfile(fullname):
+                statdict = os.stat(fullname)
+                lastmodified = datetime.datetime.fromtimestamp(statdict.st_mtime)
+                filesize = self.human_readable_size(statdict.st_size,decimal_places=2)
+            r.append('<tr>'
+                     '<td class="n"><a href="%s">%s</a></td>'
+                     '<td class="m">%s</td>'
+                     '<td class="s">%s</td>'
+                     '<td class="t">%s</td>'
+                     '</tr>'
+                    % (urllib.parse.quote(linkname,
+                                          errors='surrogatepass'),
+                       html.escape(displayname, quote=False),lastmodified,filesize,filetype))
+
+        r.append('</tbody>')
+        r.append('</div></body>\n</html>\n')
+        encoded = '\n'.join(r).encode(enc, 'surrogateescape')
+        f = io.BytesIO()
+        f.write(encoded)
+        f.seek(0)
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-type", "text/html; charset=%s" % enc)
+        self.send_header("Content-Length", str(len(encoded)))
+        self.end_headers()
+        return f
 
 
 
