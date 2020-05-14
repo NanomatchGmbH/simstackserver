1c3c9b5981a82d8c93fd1f0f0b62dd40e10f6fa8
Timo Strunk <Timo.Strunk@nanomatch.com>
2020-05-14 22:05:41 +0200

    Forgotten commit

---
+++ SimStackServer/Util/InternalBatchSystem.py
@@ -0,0 +1,14 @@
+
+class InternalBatchSystem:
+    _processfarm = None
+    _processfarm_thread = None
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_instance(cls):
+        from threadfarm.processfarm import ProcessFarm
+        if cls._processfarm is None:
+            assert cls._processfarm_thread is None
+            cls._processfarm_thread, cls._processfarm = ProcessFarm.start_processfarm_as_thread()
+        return cls._processfarm, cls._processfarm_thread
