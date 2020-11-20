07caca33c596444e7a4f73d2a15ce96c8b6fa746
Timo Strunk <Timo.Strunk@nanomatch.com>
2020-11-20 10:49:17 +0100

    setup aiida account now works for multi user

--- SimStackServer/wano_calcjob/tools/setup_aiida_account.sh
+++ SimStackServer/wano_calcjob/tools/setup_aiida_account.sh
@@ -23,12 +23,22 @@ sudo -u postgres createuser $username
 dbname="aiida_$username"
 sudo -u postgres createdb -O $username $dbname
 
+brokerpass=`pwgen 16`
+vhostname="aiida_$username"
+mquser="aiida_$username"
+
+rabbitmqctl add_user $mquser $brokerpass
+rabbitmqctl add_vhost $vhostname
+rabbitmqctl set_permissions -p $vhostname $mquser ".*" ".*" ".*"
+
+
 aiidaexec=`realpath wano-aiida-exec`
 sudo -u $username bash -c "source $NANOMATCH/$NANOVER/local_anaconda/etc/profile.d/conda.sh; conda activate aiida;\
+reentry scan;\
 verdi setup --profile $username --db-engine postgresql_psycopg2 --db-backend django --db-host \"\" --db-port 5432 \
-            --db-name $dbname --db-username $username --db-password \"\" --broker-protocol amqp --broker-username guest \
-            --broker-password guest --broker-host 127.0.0.1 --broker-port 5672 --broker-virtual-host \"\" ; \
-verdi computer setup -L local -H localhost -D \"\" -T local -S slurm -w /home/{username}/aiida_workspace --prepend-text \"echo AiiDAStartup\" --append-text \"echo AiiDAShutdown\" ; \
+            --db-name $dbname --db-username $username --db-password \"\" --broker-protocol amqp --broker-username $mquser \
+            --broker-password $brokerpass --broker-host 127.0.0.1 --broker-port 5672 --broker-virtual-host $vhostname ; \
+verdi computer setup -L local -H localhost -D \"\" -T local -S slurm -w /home/ws/{username}/aiida_workspace --prepend-text \"echo AiiDAStartup\" --append-text \"echo AiiDAShutdown\" ; \
 verdi computer configure local local; \
 reentry scan; \
 verdi daemon stop; \
