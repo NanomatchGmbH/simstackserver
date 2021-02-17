1ef29e21fe25e8926fbfe76adee7cd1c3dec96fa
Joerg Schaarschmidt <joerg.schaarschmidt@kit.edu>
2021-02-17 11:17:45 +0100

    Allow KillSimStackServer.sh to kill all processes if root

--- KillSimStackServer.sh
+++ KillSimStackServer.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# if root kill all SimStackServer processes
+if [ $(id -u) -eq 0 ]; then
+    for PID in $(pgrep -f "python .*SimStackServer"); do
+        echo "Killing SimStackServer process $PID of user $(ps -o user= -p $PID). Please wait 20 seconds for it to shutdown."
+        kill $PID
+        if  [ $? -ne 0 ]; then 
+            sleep 20 && kill -KILL $PID 2> /dev/null &
+        fi
+    done
+    exit 0
+fi
+
+# otherwise kill process for current user
 PID="$(ps x -u $USER | grep SimStackServer | grep python | grep -v grep | gawk '{print $1}')"
 if [ "AA$PID" != "AA" ]
 then
