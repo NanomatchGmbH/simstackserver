0da2d00d34a72740855ec661b915fc887c63d692
Timo Strunk <Timo.Strunk@nanomatch.com>
2021-07-19 11:36:15 +0200

    Defaulting to port 22

--- SimStackServer/ClusterManager.py
+++ SimStackServer/ClusterManager.py
@@ -35,7 +35,11 @@ class ClusterManager(object):
         """
         self._logger = logging.getLogger("ClusterManager")
         self._url = url
-        self._port = int(port)
+        try:
+            self._port = int(port)
+        except ValueError as e:
+            print("Port was set to >%s<. Using default port of 22")
+            self._port = 22
         self._calculation_basepath = calculation_basepath
         self._extra_config = extra_config
         self._user = user
