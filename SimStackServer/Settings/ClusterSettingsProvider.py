8757f6020b6fba8bd3b3b1123b96290ef240e18a
Timo Strunk <Timo.Strunk@nanomatch.com>
2022-03-25 14:02:35 +0100

    Added method to remove clusterresources, add method to physically delete

--- SimStackServer/Settings/ClusterSettingsProvider.py
+++ SimStackServer/Settings/ClusterSettingsProvider.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
 
-from SimStackServer.Util.ClusterSettings import get_cluster_settings_from_folder, save_cluster_settings_to_folder
+from SimStackServer.Util.ClusterSettings import get_cluster_settings_from_folder, save_cluster_settings_to_folder, \
+    remove_clustersettings_from_folder
 
 
 class ClusterSettingsProvider:
@@ -27,6 +28,15 @@ class ClusterSettingsProvider:
     @classmethod
     def get_registries(cls):
         return cls.get_instance()._settings_container
+
+    @classmethod
+    def remove_resource(cls, resource_name: str) -> None:
+        instance = cls.get_instance()
+        sc = instance._settings_container
+        folder = instance._get_settings_folder()
+        remove_clustersettings_from_folder(folder, resource_name)
+        if resource_name in sc:
+            del sc[resource_name]
 
     def _parse_settings(self):
         self._settings_container = get_cluster_settings_from_folder(self._get_settings_folder())
--- SimStackServer/Util/ClusterSettings.py
+++ SimStackServer/Util/ClusterSettings.py
@@ -3,10 +3,23 @@ import pathlib
 from SimStackServer.WorkflowModel import Resources
 
 
+___clustersettings_file_ending = ".clustersettings"
+
+def get_clustersettings_filename_from_folder(folder: pathlib.Path, clustername: str) -> pathlib.Path:
+    global ___clustersettings_file_ending
+    return folder/ f"{clustername}{___clustersettings_file_ending}"
+
+def remove_clustersettings_from_folder(folder: pathlib.Path, clustername: str) -> None:
+    filename = get_clustersettings_filename_from_folder(folder, clustername)
+    filename.unlink(missing_ok=True)
+
 def get_cluster_settings_from_folder(folder: pathlib.Path):
+    global ___clustersettings_file_ending
     outdict = {}
     for file in folder.iterdir():
         if not file.is_file():
+            continue
+        elif not str(file).endswith(___clustersettings_file_ending):
             continue
         else:
             name = file.stem
@@ -16,6 +29,7 @@ def get_cluster_settings_from_folder(folder: pathlib.Path):
     return outdict
 
 def save_cluster_settings_to_folder(folder: pathlib.Path, clustersettings: dict):
+    global ___clustersettings_file_ending
     for name, resources in clustersettings.items():
-        outpath = folder/ f"{name}.json"
+        outpath = get_clustersettings_filename_from_folder(folder, name)
         resources.to_json(outpath)
