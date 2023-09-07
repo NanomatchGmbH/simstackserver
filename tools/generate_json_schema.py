97fe1a993f5d1c434a102ce7b7d23f99a8cd7159
Timo Strunk <Timo.Strunk@gmail.com>
2023-09-07 23:49:41 +0200

    Add additionalProperties everywhere in json schema generator

--- tools/generate_json_schema.py
+++ tools/generate_json_schema.py
@@ -1,6 +1,20 @@
 import yaml
 import json
 from genson import SchemaBuilder
+from TreeWalker.TreeWalker import TreeWalker
+
+def add_additional_properties(subdict):
+    sd_type = subdict.get("type", "not_object")
+    if sd_type == "object":
+        subdict["additionalProperties"] = False
+        walker = TreeWalker(subdict["properties"])
+        subdict["properties"] = walker.walker(
+            path_visitor_function=None,
+            data_visitor_function=None,
+            subdict_visitor_function = add_additional_properties, capture=True)
+        return subdict
+    return None
+
 
 if __name__ == '__main__':
     with open("output_dict.yml", "r") as infile:
@@ -9,4 +23,10 @@ if __name__ == '__main__':
     builder = SchemaBuilder()
     builder.add_object(content)
 
-print(json.dumps(builder.to_schema(), indent=2))
+    no_add_properties_schema = builder.to_schema()
+    tw = TreeWalker(no_add_properties_schema)
+    add_properties_schema = tw.walker(path_visitor_function=None,
+              data_visitor_function=None,
+              subdict_visitor_function=add_additional_properties,
+              capture=True)
+    print(json.dumps(add_properties_schema, indent=2))
