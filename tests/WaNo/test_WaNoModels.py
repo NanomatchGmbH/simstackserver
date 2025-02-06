78f9bfcdf6f26157dab9ad0c63f2f848b50103ce
Tobias Neumann <tobias.neumann@nanomatch.com>
2025-02-06 17:00:44 +0100

    WaNoBool test and small fixes bc lint complained

--- tests/WaNo/test_WaNoModels.py
+++ tests/WaNo/test_WaNoModels.py
@@ -1,6 +1,9 @@
-from numpy.lib.recfunctions import apply_along_fields
-
-from SimStackServer.WaNo.WaNoModels import WaNoItemStringModel, WaNoThreeRandomLetters, WaNoItemIntModel
+from SimStackServer.WaNo.WaNoModels import (
+    WaNoItemStringModel,
+    WaNoThreeRandomLetters,
+    WaNoItemIntModel,
+    WaNoItemBoolModel,
+)
 from xml.etree.ElementTree import fromstring
 
 
@@ -15,16 +18,39 @@ def test_WaNoItemIntModel():
     assert wiim.get_data() == 2
     wiim.set_data(3)
     assert wiim.get_data() == 3
-    assert wiim.get_secure_schema() == {'key': {'type': 'int'}}
+    assert wiim.get_secure_schema() == {"key": {"type": "int"}}
     assert wiim.get_delta_to_default() == 3
     wiim.apply_delta(4)
     assert wiim.get_data() == 4
-    assert wiim.changed_from_default() == True
-    assert wiim.get_type_str() == 'Int'
+    assert wiim.changed_from_default() is True
+    assert wiim.get_type_str() == "Int"
     wiim.set_data(13)
-    wiim.update_xml() # how to test?
-    assert wiim.xml.text == '13'
-    assert repr(wiim) == '13'
+    wiim.update_xml()
+    assert wiim.xml.text == "13"
+    assert repr(wiim) == "13"
+
+
+def test_WaNoItemBoolModel():
+    wibm = WaNoItemBoolModel()
+    xml = fromstring(
+        """
+                <WaNoBool name="key">False</WaNoBool>
+            """
+    )
+    wibm.parse_from_xml(xml)
+    assert wibm.get_data() is False
+    wibm.set_data(True)
+    assert wibm.get_data() is True
+    assert wibm.get_secure_schema() == {"key": {"type": "boolean"}}
+    assert wibm.get_delta_to_default() is True
+    wibm.apply_delta(False)
+    assert wibm.get_data() is False
+    wibm.set_data(True)
+    assert wibm.changed_from_default() is True
+    assert wibm.get_type_str() == "Boolean"
+    wibm.set_data(False)
+    wibm.update_xml()
+    assert wibm.xml.text == "False"
 
 
 def test_WaNoItemStringModel():
