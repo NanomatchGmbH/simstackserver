d3f95705714a1d94f031ee06f198861ca3200651
Tobias Neumann <tobias.neumann@nanomatch.com>
2025-03-11 16:38:25 +0100

    lint

--- tests/test_EvalHandler.py
+++ tests/test_EvalHandler.py
@@ -1,4 +1,7 @@
-from SimStackServer.EvalHandler import eval_numpyexpression  # adjust the import path accordingly
+from SimStackServer.EvalHandler import (
+    eval_numpyexpression,
+)  # adjust the import path accordingly
+
 
 def test_eval_numpyexpression_array():
     result = eval_numpyexpression("4+2")
