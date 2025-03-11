445241134ca038f9ca3e627e8305a498ae45dae0
Tobias Neumann <tobias.neumann@nanomatch.com>
2025-03-11 16:39:02 +0100

    lint

--- tests/Util/test_ClientResourceModelToServerResourceModel.py
+++ tests/Util/test_ClientResourceModelToServerResourceModel.py
@@ -2,13 +2,20 @@ from unittest.mock import patch
 
 import yaml
 
-from SimStackServer.Util.ClientResourceModelToServerResourceModel import ResourceROWTYPE, get_default_resource_list, load_resource_list, ClientResourceModelToServerResourceModel
+from SimStackServer.Util.ClientResourceModelToServerResourceModel import (
+    ResourceROWTYPE,
+    get_default_resource_list,
+    load_resource_list,
+    ClientResourceModelToServerResourceModel,
+)
+
 
 def test_ResourceROWTYPE():
     rrt = ResourceROWTYPE(0)
     assert rrt.ppn == 0
     rrt = ResourceROWTYPE(4)
     assert rrt.queue == 4
+
 
 def test_get_default_resource_list():
     target_list = [
@@ -23,6 +30,7 @@ def test_get_default_resource_list():
     for t in target_list:
         assert t in mylist
 
+
 def test_load_resource_list(tmpfile):
     update_list = [
         [False, "CPUs per Node", 2],
@@ -34,6 +42,7 @@ def test_load_resource_list(tmpfile):
     returned_list = load_resource_list(tmpfile)
     assert returned_list[ResourceROWTYPE.ppn][2] == 2
     assert returned_list[ResourceROWTYPE.queue][2] == "night"
+
 
 def test_ClientResourceModelToServerResourceModel():
     my_client_list = get_default_resource_list()
@@ -50,8 +59,15 @@ def test_ClientResourceModelToServerResourceModel():
 
     with patch("SimStackServer.WorkflowModel.Resources", side_effect=mock_resources):
         returned_args = ClientResourceModelToServerResourceModel(my_client_list)
-        assert returned_args == (16, 1, 1234, 12345, 'somequeue', 'more_colakracher')
+        assert returned_args == (16, 1, 1234, 12345, "somequeue", "more_colakracher")
 
         my_client_list[ResourceROWTYPE.queue][0] = False
         returned_args = ClientResourceModelToServerResourceModel(my_client_list)
-        assert returned_args == (16, 1, 1234, 12345, '${QUEUE_NAME}', 'more_colakracher')
+        assert returned_args == (
+            16,
+            1,
+            1234,
+            12345,
+            "${QUEUE_NAME}",
+            "more_colakracher",
+        )
